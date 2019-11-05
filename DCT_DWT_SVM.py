99ef5f0a72aa4ca2f6f6d0a68ad85e972918ebb3
Adrian Metcalf <Adrian.metcalf85@gmail.com>
2019-11-04 19:22:28 -0600

    intiial commit

---
+++ DCT_DWT_SVM.py
@@ -0,0 +1,14 @@
+from scipy.fftpack import dct
+from pywt import dwt
+from sklearn import svm
+
+# use dct scipy.fftpack.dct
+# https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
+
+# use dwt: pywt.dwt()
+# https://pywavelets.readthedocs.io/en/latest/ref/dwt-discrete-wavelet-transform.html
+
+# use SVM: scikitlearn
+# clf = svm.SVC(gamma='scale')
+# clf.fit(x,y)
+# https://scikit-learn.org/stable/modules/svm.html
---
+++ PCA_CNN.py
@@ -0,0 +1,6 @@
+from sklearn.decomposition import PCA
+
+# do PCA -> pca = PCA(n_components = 2)
+# pca.fit(x) where x is a numpy array
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+
