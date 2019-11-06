0d4ee419413c45de606aa5992bdf93d55287d04f
Adrian Metcalf <Adrian.metcalf85@gmail.com>
2019-11-06 16:06:38 -0600

    Loaded Data

--- DCT_DWT_SVM.py
+++ DCT_DWT_SVM.py
@@ -1,6 +1,9 @@
 from scipy.fftpack import dct
 from pywt import dwt
 from sklearn import svm
+import torch
+import numpy as np
+import emnist
 
 # use dct scipy.fftpack.dct
 # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
@@ -12,3 +15,6 @@ from sklearn import svm
 # clf = svm.SVC(gamma='scale')
 # clf.fit(x,y)
 # https://scikit-learn.org/stable/modules/svm.html
+
+
+def load_data():
--- test.py
+++ test.py
@@ -2,3 +2,5 @@
 # the primary focus is to compare efficiency, so getting certain performance characteristics and maybe
 # mathetmatical approximations for performance (taylor series, for example to find an equation that makes an
 # appropriate solution
+
+import emnist
