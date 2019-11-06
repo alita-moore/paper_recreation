335c7f762e9f02d9e4b4b3fbacb036ef14034191
Adrian Metcalf <Adrian.metcalf85@gmail.com>
2019-11-06 17:28:05 -0600

    created load_data()

--- DCT_DWT_SVM.py
+++ DCT_DWT_SVM.py
@@ -11,10 +11,34 @@ import emnist
 # use dwt: pywt.dwt()
 # https://pywavelets.readthedocs.io/en/latest/ref/dwt-discrete-wavelet-transform.html
 
-# use SVM: scikitlearn
+# use SVM: scikit-learn
 # clf = svm.SVC(gamma='scale')
 # clf.fit(x,y)
 # https://scikit-learn.org/stable/modules/svm.html
 
 
 def load_data():
+    load_data.__doc__ = ("A function that loads pre-processed data into a numpy array \n"
+                         " \n"
+                         "Outputs 8x(dimension of data) tuple, which follows:\n"
+                         "[EMNIST train, EMNIST test, EMNIST train label, EMNIST test label...\n"
+                         ", MNIST train, MNIST test, MNIST train labels, MNIST test labels")
+
+    dirs = ['./data/EMNIST/sample/training_processed.pt',
+                 './data/EMNIST/sample/test_processed.pt',
+                 './data/EMNIST/label/train_label_processed.pt',
+                 './data/EMNIST/label/test_label_process.pt',
+                 './data/MNIST/processed/training_processed.pt',
+                 './data/MNIST/processed/test_processed.pt',
+                 './data/MNIST/processed/train_label_processed.pt',
+                 './data/MNIST/processed/test_label_process.pt']
+    # load tensors
+    Data = [torch.load(item) for item in dirs]
+
+    # transform into squeezed np arrays
+    Data = [np.squeeze(np.asarray(item)) for item in Data]
+
+    return Data
+
+
+data = load_data()  # np array squeezed because other parts are not necessary
