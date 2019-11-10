89d65c183f6aed6f842c24523464556d76779c7a
Adrian Metcalf <Adrian.metcalf85@gmail.com>
2019-11-10 14:24:57 -0600

    updated file structure

--- test.py
+++
@@ -1,7 +0,0 @@
-# the purpose of this file is to test a given result and to return necessary statistical results
-# the primary focus is to compare efficiency, so getting certain performance characteristics and maybe
-# mathetmatical approximations for performance (taylor series, for example to find an equation that makes an
-# appropriate solution
-
-
-
